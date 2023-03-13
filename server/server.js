4741b5185f8eaaf57f4aaca4746860b22f980af8
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-03-13 10:32:23 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import express from 'express';
+import mongoose from 'mongoose';
+import url from 'url';
+import path from 'path';
+
+
+const app = express();
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.urlencoded({ extended: true }));
+
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  };
+}
+
+connect();
+
+
+app.listen(process.env.PORT);
