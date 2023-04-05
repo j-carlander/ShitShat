3bad76bc4366b9f6b53159372a154ba41a33bad5
j-carlander <johannes.carlander@gmail.com>
2023-04-05 16:31:18 +0200

    added db to reg user

--- backend/express/src/mongo/mongoDB.js
+++ backend/express/src/mongo/mongoDB.js
@@ -1,30 +1,31 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import url from 'url';
-import { MongoClient } from 'mongodb';
+import dotenv from "dotenv";
+import path from "path";
+import url from "url";
+import { MongoClient } from "mongodb";
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-dotenv.config({path:__dirname+'../../../../.env'});
+// dotenv.config({ path: __dirname + "../../../../.env" });
+dotenv.config();
 
 let dbConnection = undefined;
 
 export function fetchCollection(name) {
   return connect().collection(name);
-};
-
-async function connect() {
-if(dbConnection == undefined){
-  return dbConnection
 }
 
+function connect() {
+  if (dbConnection != undefined) {
+    return dbConnection;
+  }
+
   try {
-    const client =  await MongoClient.connect(process.env.URI);
-    dbConnection = client.db('ShitShat');
+    const client = new MongoClient(process.env.URI);
+    dbConnection = client.db("ShitShat");
     console.log("Connected to MongoDB Atlas, ShitShat");
     return dbConnection;
   } catch (error) {
     console.error(error);
-  };
-};
+  }
+}
