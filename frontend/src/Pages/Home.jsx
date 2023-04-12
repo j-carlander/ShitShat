5ad6830dd77a82a017fda79ac11312b24446f200
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-04-12 10:21:49 +0200

    started on homepage

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import "./Home.css";
+import React, { useEffect } from "react";
+import "./Styles/Home.css";
+
+async function fetchRoomTitles() {
+  const result = await fetch("http://localhost:4500/api/channel");
+
+  return await result.json();
+}
+
+const roomTitles = fetchRoomTitles();
 
 export default function Home() {
   return (
     <div className="home-container">
       <section className="home-main">
-        
+        <secition>
+          <ul>
+            {roomTitles.map((title) => {
+              return <li>{title.title}</li>;
+            })}
+          </ul>
+        </secition>
       </section>
     </div>
   );
